1a243739d51931b139aa008f48408be23b5132d4
Pivinter <70973362+Pivinter@users.noreply.github.com>
2022-09-29 15:55:24 +0300

    Add project files.

---
+++ Testlab41/main.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int N, i;
+	double S;
+	cout << "N = "; cin >> N;
+	if (N <= 22) {
+		//1)
+		S = 0;
+		i = N;
+		while (i <= 22)
+		{
+			S += sqrt(i * i + N * N) / i;
+			i++;
+		}
+		cout << S << endl;
+
+		//2)
+		S = 0;
+		i = N;
+		do {
+			S += sqrt(i * i + N * N) / i;
+			i++;
+		} while (i <= 22);
+		cout << S << endl;
+
+		//3)
+		S = 0;
+		for (i = N; i <= 22; i++)
+		{
+			S += sqrt(i * i + N * N) / i;
+		}
+		cout << S << endl;
+
+		//4)
+		S = 0;
+		for (i = 22; i >= N; i--)
+		{
+			S += sqrt(i * i + N * N) / i;
+		}
+		cout << S << endl;
+	}
+	else cout << "The number is greater than 22" << endl;
+	return 0;
+}
